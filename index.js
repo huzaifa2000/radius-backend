fa6b7f12ef92cd3f905aa5aece00acb5525e4528
huzaifa <huzaifapk2000@gmail.com>
2023-01-04 06:22:54 +0500

    fixed

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 5000
+const PORT = process.env.PORT || 5000;
 app.use(express.json())
 
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
 })
 

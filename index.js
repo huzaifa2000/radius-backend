891e85fb017bb0e8a373a574a9821894f3845f54
huzaifa <huzaifapk2000@gmail.com>
2023-01-04 06:56:09 +0500

    port issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.use(express.json())
 
 //Available Routes
